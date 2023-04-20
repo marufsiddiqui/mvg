83d1da6b6683e8a5377a5a16cac7677bf1d58aa3
Maruf Siddiqui <siddiquim@mediamarktsaturn.com>
2023-04-20 19:05:43 +0200

    Getting it to work

--- mvg1/Api.swift
+++ mvg1/Api.swift
@@ -1,29 +1,22 @@
-//
-//  Api.swift
-//  mvg1
-//
-//  Created by Maruf Siddiqui on 20.04.23.
-//
-
 import Foundation
 
-struct Departure: Decodable {
-    let label: String
-    let realtimeDepartureTime: String
-}
-
-class Api {
-    static func fetchDepartures() async -> [Departure] {
-        do {
-            let url = URL(string: "https://www.mvg.de/api/fib/v2/departure?globalId=de:09162:1464&limit=10&offsetInMinutes=0&transportTypes=BUS")!
-            let (data, _) = try await URLSession.shared.data(from: url)
-            let decoder = JSONDecoder()
-            decoder.keyDecodingStrategy = .convertFromSnakeCase
-            let response = try decoder.decode([Departure].self, from: data)
-            return response
-        } catch {
-            print("Error fetching departures: \(error.localizedDescription)")
-            return []
-        }
-    }
-}
+//struct Departure: Decodable {
+//    let label: String
+//    let realtimeDepartureTime: String
+//}
+//
+//class Api {
+//    static func fetchDepartures() async -> [Departure] {
+//        do {
+//            let url = URL(string: "https://www.mvg.de/api/fib/v2/departure?globalId=de:09162:1464&limit=10&offsetInMinutes=0&transportTypes=BUS")!
+//            let (data, _) = try await URLSession.shared.data(from: url)
+//            let decoder = JSONDecoder()
+//            decoder.keyDecodingStrategy = .convertFromSnakeCase
+//            let response = try decoder.decode([Departure].self, from: data)
+//            return response
+//        } catch {
+//            print("Error fetching departures: \(error.localizedDescription)")
+//            return []
+//        }
+//    }
+//}
--- mvg1/mvg1App.swift
+++ mvg1/mvg1App.swift
@@ -1,17 +1,63 @@
-//
-//  mvg1App.swift
-//  mvg1
-//
-//  Created by Maruf Siddiqui on 20.04.23.
-//
-
 import SwiftUI
 import UserNotifications
+
+
+//struct Departure: Decodable {
+//    let label: String
+//    let realtimeDepartureTime: String
+//
+//    enum CodingKeys: String, CodingKey {
+//        case label = "label"
+//        case realtimeDepartureTime = "realtimeDepartureTime"
+//    }
+//}
+
+struct Departure: Codable {
+    //    let bannerHash: String
+    //    let cancelled: Int
+    //    let delayInMinutes: Int
+        let destination: String
+    let label: String
+    //    let messages: [String]
+    //    let network: String
+    //    let occupancy: String
+    //    let plannedDepartureTime: Int
+    //    let realtime: Int
+        let realtimeDepartureTime: Int
+    //    let sev: Int
+    //    let stopPointGlobalId: String
+    //    let trainType: String
+    //    let transportType: String
+}
 
 @main
 struct mvg1App: App {
     // 1
     @State var currentNumber: String = "1"
+    @State var departures: [Departure] = []
+    
+    func fetchDepartures(completion: @escaping ([Departure]?) -> Void) {
+        guard let url = URL(string: "https://www.mvg.de/api/fib/v2/departure?globalId=de:09162:1464&limit=10&offsetInMinutes=0&transportTypes=BUS") else {
+            completion(nil)
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let data = data {
+                let decoder = JSONDecoder()
+                do {
+                    let departures = try decoder.decode([Departure].self, from: data)
+                    completion(departures)
+                } catch {
+                    completion(nil)
+                }
+            } else if let error = error {
+                print(error.localizedDescription)
+                completion(nil)
+            }
+        }
+        task.resume()
+    }
     
     var body: some Scene {
         WindowGroup {
@@ -29,45 +75,44 @@ struct mvg1App: App {
                     }
                     
                     // Make an API call to retrieve data from https://rickandmortyapi.com/api/character/158
-                    let url = URL(string: "https://rickandmortyapi.com/api/character/158")!
+                    let url = URL(string: "https://www.mvg.de/api/fib/v2/departure?globalId=de:09162:1464&limit=10&offsetInMinutes=0&transportTypes=BUS")!
                     let task = URLSession.shared.dataTask(with: url) { data, response, error in
                         if let data = data {
                             let decoder = JSONDecoder()
-                            if let character = try? decoder.decode(Character.self, from: data) {
-                                // Set the notification content
-                                let content = UNMutableNotificationContent()
-                                content.title = "My Menu App"
-                                content.body = "The character \(character.name) is waiting for you!"
-                                content.sound = UNNotificationSound.default
-                                // Set the notification request
-                                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: nil)
-                                notificationCenter.add(request) { error in
-                                    if let error = error {
-                                        print("Error: \(error.localizedDescription)")
-                                    } else {
-                                        print("Notification sent!")
+                            do {
+                                let departures = try decoder.decode([Departure].self, from: data)
+                                let aidenbachDepartures = departures.filter { $0.destination == "Aidenbachstraße" }
+
+                                
+                                if let departure = aidenbachDepartures.first {
+                                    // Set the notification content
+                                    let content = UNMutableNotificationContent()
+                                    let timestamp = departure.realtimeDepartureTime
+                                    let date = Date(timeIntervalSince1970: TimeInterval(timestamp/1000))
+                                    let formatter = DateFormatter()
+                                    formatter.dateFormat = "HH:mm:ss"
+                                    let dateString = formatter.string(from: date)
+                                    content.title = "Bus Departure"
+                                    content.body = "The next bus \(departure.label) is leaving at \(dateString) from \(departure.label)"
+                                    content.sound = UNNotificationSound.default
+                                    // Set the notification request
+                                    let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: nil)
+                                    notificationCenter.add(request) { error in
+                                        if let error = error {
+                                            print("Error: \(error.localizedDescription)")
+                                        } else {
+                                            print("Notification sent!")
+                                        }
                                     }
                                 }
+                            } catch {
+                                print("Error decoding data: \(error.localizedDescription)")
                             }
                         } else if let error = error {
                             print("Error: \(error.localizedDescription)")
                         }
                     }
                     task.resume()
-                    
-//                    // Show a notification with the message "Hello World"
-//                    let content = UNMutableNotificationContent()
-//                    content.title = "Hello World"
-//                    content.body = "Welcome to my app"
-//                    content.sound = UNNotificationSound.default
-//                    
-//                    let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: nil)
-//                    
-//                    UNUserNotificationCenter.current().add(request) { error in
-//                        if let error = error {
-//                            print("Failed to add notification: \(error.localizedDescription)")
-//                        }
-//                    }
                 }
         }
         
