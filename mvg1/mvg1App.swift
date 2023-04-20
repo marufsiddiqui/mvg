148e72ffd84dd9e01d3b5946404a6aa29bbee885
Maruf Siddiqui <siddiquim@mediamarktsaturn.com>
2023-04-20 11:05:20 +0200

    Initial Commit

---
+++ mvg1/mvg1App.swift
@@ -0,0 +1,17 @@
+//
+//  mvg1App.swift
+//  mvg1
+//
+//  Created by Maruf Siddiqui on 20.04.23.
+//
+
+import SwiftUI
+
+@main
+struct mvg1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
